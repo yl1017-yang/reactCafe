785b23c5e615524073334035d7fec0eb2ad04ebb
yanglae <yanglae@hanmail.net>
2023-01-03 16:49:39 +0900

    프로그래스바

--- client/src/pages/Main.js
+++ client/src/pages/Main.js
@@ -1,60 +1,111 @@
+import React from 'react';
 import { Row, Col } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import "./Main.css";
 
-function Main() {
+// import jquery from 'jquery';
+import $ from "jquery";
 
-  return (
-    <>
-      <Row>
-        <Col><h2 className="title-h2">메인</h2></Col>
-        <Col className="col-auto"><Link className="btn btn-outline-primary" role="button" to="BoardList">게시판</Link></Col>
-      </Row>
+class Main extends React.Component {
+// function Main() {
 
-      <div>
-        <p>리액트 client/server</p>
-        <p>게시판 리스트 : 게시판 리스트,글쓰기,상세</p>
-        <p>상품 리스트 : 상품 리스트,글쓰기,상세</p>
-      </div>
+  componentDidMount(){
+    $(function() {
+      $('.gauge').each(function () {
+        var $this = $(this);
+        var value = $this.attr('value');
+        $this.animate({
+            width: value + "%"
+        }, 1000, 'linear' );
 
-      {/* <Card className="card-con mt-2 mb-4">
-        <Card.Body>
-          <Card.Title>최근 1년동안 매출 추이</Card.Title>
-          <Card.Text>당월예상매출액 : 50건 <span>500,000원</span></Card.Text>
-        </Card.Body>
-      </Card>
+       $this.text(value);
+      });
+    });
+  }
 
-      <div className="tbl-type1 table-responsive">
-        <Table hover>
-          <thead>
-            <tr>
-              <th>당일</th>
-              <th>1월판매 (1~말일)</th>
-              <th>2월판매 (1~말일)</th>
-              <th>3월판매 (1~말일)</th>
-              <th>2월일평균 (1~말일)</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td><span className="txt-exclu-case">17건</span></td>
-              <td><span className="txt-exclu-case">7건</span></td>
-              <td><span className="txt-exclu-case">1건</span></td>
-              <td><span className="txt-exclu-case">25건</span></td>
-              <td><span className="txt-exclu-case">37건</span></td>
-            </tr>
-            <tr>
-              <td><span className="txt-exclu-price">988,385원</span></td>
-              <td><span className="txt-exclu-price">200,000원</span></td>
-              <td><span className="txt-exclu-price">180,300원</span></td>
-              <td><span className="txt-exclu-price">300,385원</span></td>
-              <td><span className="txt-exclu-price">450,385원</span></td>
-            </tr>
-          </tbody>
-        </Table>
-      </div> */}
+  pressClick = () => {
+    document.getElementById( 'press' ).value = '100';
+  }
+  pressClick2 = () => {
+    document.getElementById( 'press' ).value = '20';
+  }
+  meterClick = () => {
+    document.getElementById( 'meter' ).value = '100';
+  }
+  meterClick2 = () => {
+    document.getElementById( 'meter' ).value = '0';
+  }
+  gaugeClick = () => {
+    $(".gauge").val("");
+  }
 
-    </>
-  )
+  render(){
+    return (
+      <>
+        <Row>
+          <Col><h2 className="title-h2">메인</h2></Col>
+          <Col className="col-auto"><Link className="btn btn-outline-primary" role="button" to="BoardList">게시판</Link></Col>
+        </Row>
+
+        <p>프로그래스바 게이지</p>
+
+
+        
+
+        <div className="progress-box">
+          <ul>
+            <li>
+              <label>사용량 10%</label>
+              <progress min="0" max="100" value="10" id="press" className="press-move"></progress>
+              <button onClick={ this.pressClick } className="btn">100% 클릭</button>
+              <button onClick={ this.pressClick2 } className="btn">20% 클릭</button>
+            </li>
+            <li>
+              <label>사용량 10%</label>
+              <meter min="0" max="100" value="10" low="30" high="70" optimum="100" id="meter"></meter>
+              {/* 전체 100 중에서 64만큼 차지합니다.*/}
+              <button onClick={ this.meterClick } className="btn">100% 클릭</button>
+              <button onClick={ this.meterClick2 } className="btn">0% 클릭</button>
+            </li>
+          </ul>
+
+          <ul>
+            <li>
+              <div className="gauge-box">
+                <input type="text" name="txt" value="10" className="gauge" />
+              </div>
+              {/* <button onClick={ this.gaugeClick } className="btn">클릭</button> */}
+
+              <div className="gauge-box">
+                <input type="text" name="txt" value="30" className="gauge" />
+              </div>
+              <div className="gauge-box">
+                <input type="text" name="txt" value="60" className="gauge" />
+              </div>
+              <div className="gauge-box">
+                <input type="text" name="txt" value="80" className="gauge" />
+              </div>
+              <div className="gauge-box">
+                <input type="text" name="txt" value="100" className="gauge" />
+              </div>
+            </li>
+            <li>
+            </li>
+          </ul>
+        </div>
+
+ 
+
+       
+
+
+        <div>
+          참고 : https://blog.naver.com/PostView.naver?blogId=home124&logNo=222703044367
+        </div>
+
+      </>
+    );
+  }
 }
 
 export default Main;
